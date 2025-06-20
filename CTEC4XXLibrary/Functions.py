c323cf08404215a6de5df1e8589764f9f7f34ff1
User10166 <rduke01@nyit.edu>
2025-06-20 09:36:20 -0400

    Update Functions.py

--- CTEC4XXLibrary/Functions.py
+++ CTEC4XXLibrary/Functions.py
@@ -1,1 +1,172 @@
+import numpy as np
+import pandas as pd
+import sys
+from docx import Document
+from docx.shared import Pt
+from docx.enum.section import WD_ORIENT
+from docx.shared import Inches
+import matplotlib.pyplot as plt
+
+stdoutInstance = sys.stdout
+
+class _WordDocument():
+    def __init__(self, file):
+        self.file = file + ".docx";
+        self.document = Document()
+        sections = self.document.sections
+        for section in sections:
+            section.top_margin = Inches(0.5)
+            section.bottom_margin = Inches(0.5)
+            section.left_margin = Inches(0.5)
+            section.right_margin = Inches(0.5)
+            section.orientation = WD_ORIENT.LANDSCAPE
+            new_width, new_height = section.page_height, section.page_width
+            section.page_width = new_width
+            section.page_height = new_height
+        style = self.document.styles['No Spacing']
+        font = style.font
+        font.name = 'Courier New'
+        font.size = Pt(10)
+        self.text = ''
+
+    def writeText(self, text):
+        if self.text == '':
+            self.text = text
+        else:
+            self.text = self.text + text
+
+    def saveDocument(self):
+        paragraph = self.document.add_paragraph(self.text)
+        paragraph.style = self.document.styles['No Spacing']
+        self.document.save(self.file)
+
+class _Logger(object):
+    def __init__(self, file):
+        self.terminal = sys.stdout
+        self.wordDocument = _WordDocument(file)
+   
+    def write(self, message):
+        self.terminal.write(message)
+        self.wordDocument.writeText(message)
+
+    def closeLog(self):
+        self.wordDocument.saveDocument()
+
+    def flush(self):
+        # this flush method is needed for python 3 compatibility.
+        # this handles the flush command by doing nothing.
+        # you might want to specify some extra behavior here.
+        pass
+
+    def __del__(self):
+        self.closeLog()
+        sys.stdout = stdoutInstance
+
+def enableLogging(file):
+    sys.stdout = _Logger(file)
+
+def disableLogging(file):
+    sys.stdout.closeLog()
+    sys.stdout = stdoutInstance
+    
+def printTable(array, columnNames=None):
+    arrayShape = array.shape
+    if len(arrayShape) == 1:
+        array = np.reshape(array, (1, arrayShape[0]))
+        arrayShape = array.shape
+    if columnNames is None:
+        if len(arrayShape) == 1:
+            numRows = int(arrayShape[0])
+            blankRows = ['' for i in range(numRows)]
+            df = pd.DataFrame(array, index=blankRows, columns=[''])
+        elif len(arrayShape) > 1:
+            numRows = int(arrayShape[0])
+            blankRows = ['' for i in range(numRows)]
+            numColumns = int(arrayShape[1])
+            blankColumns = ['' for i in range(numColumns)]
+            df = pd.DataFrame(array, index=blankRows, columns=blankColumns)
+    else:
+        numRows = int(arrayShape[0])
+        blankRows = ['' for i in range(numRows)]
+        df = pd.DataFrame(array, index=blankRows, columns=columnNames)
+    pd.set_option("display.max_rows", None)
+    pd.set_option("display.max_columns", None)
+    pd.set_option('display.expand_frame_repr', False)
+    print(df)
+    print("")
+    
+def printVariable(label, var, columnNames=None):
+    if type(var) is np.ndarray:
+        print(label + ":")
+        printTable(var, columnNames)
+    elif type(var) is list:
+        print(label + ":")
+        for item in var:
+            print("\t", item)
+        print("")
+    else:
+        print(label + ": " + str(var))
+
+def readExcelSpreadsheet(file, useColumnNames=True, useRowLabels=False):
+    if useColumnNames:
+        headerIndex = 0
+    else:
+        headerIndex = None
+
+    if useRowLabels:
+        rowIndex = 0
+    else:
+        rowIndex = None
+    data = pd.read_excel(file, header=headerIndex, index_col=rowIndex)
+    dataNumbers = data.to_numpy()
+    indexList = list(data.index)
+    namesList = list(data.columns)
+
+    return dataNumbers, indexList, namesList
+
+def encodeLabels(YRaw):
+  data = pd.DataFrame(YRaw)
+  indexList = list(data)
+  uniqueIndexesList = list(data.unique())
+  firstCategory = indexList[0]
+  try:
+    int(firstCategory)
+    Y = np.array([int(idx) for idx in indexList])
+  except ValueError:
+    Y = np.array([uniqueIndexesList.index(idx) for idx in indexList])
+    namesList = list(data.columns)
+  return Y, uniqueIndexesList
+
+def convertArray(var):
+  if type(var) is np.ndarray:
+    return np.array_str(var)
+  return str(var)
+
+def convertList(var):
+  if type(var) is list:
+    listString = ' '.join(map(str, var))
+    return listString
+  return str(var)
+
+def getPrompt(*args):
+  prompt = ''
+  for i in range(len(args)):
+    arg = args[i]
+    if i < len(args) - 1:
+      if type(arg) is np.ndarray:
+        prompt = prompt + np.array_str(arg) + ' '
+      elif type(arg) is list:
+        prompt = prompt + convertList(arg) + ' '
+      else:
+        prompt = prompt + str(arg) + ' '
+    else:
+      if type(arg) is np.ndarray:
+        prompt = prompt + np.array_str(arg)
+      elif type(arg) is list:
+        prompt = prompt + convertList(arg)
+      else:
+        prompt = prompt + str(arg)
+  return prompt
+      
+  
 
