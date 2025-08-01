5839dbe3d990363134c023040c5bd5c1936dfebd
User10166 <rduke01@nyit.edu>
2025-08-01 06:54:53 -0400

    Update __init__.py

--- CTEC405Library/__init__.py
+++ CTEC405Library/__init__.py
@@ -1,2 +1,4 @@
 from .Functions import enableLogging, disableLogging, printTable, printVariable, readExcelSpreadsheet, encodeLabels, readImages, getPrompt
 from .Architectures import feedForwardNN
+import os
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
